21bc511e9dd5583a74c8a4c54d05763e7a7c4084
David646464 <david.sanchez.peso.64@gmail.com>
2023-09-26 14:16:06 +0200

    commit

--- UD01/src/expresiones/Ejercicio1.java
+++ UD01/src/expresiones/Ejercicio1.java
@@ -9,7 +9,6 @@ public class Ejercicio1 {
         necesarios.
         */
         //a)
-        
         System.out.println("a)");
         System.out.println("3 / 2 + 4 / 3");
         //b)
@@ -50,18 +49,26 @@ public class Ejercicio1 {
         algebráicas. Coloca paréntesis 
         solamente donde sean necesarios */
         //a)
-
+        System.out.println("a)");
+        System.out.println("b / 2 - 4 * a * c");
         //b)
-
+        System.out.println("b)");
+        System.out.println("3 * x * y - 5 * x + 12 * x - 17");
         //c)
-
+        System.out.println("c)");
+        System.out.println("(b + d) / (c + 4)");
         //d)
-
+        System.out.println("d)");
+        System.out.println(" x * y / y + 2");
         //e)
-
+        System.out.println("e)");
+        System.out.println(" 1 / y + 3 * x / y + 1");
         //f)
-
+        System.out.println("f)");
+        System.out.println("1 / (y + 3) + x / y");
         //g)
+        System.out.println("g)");
+        System.out.println("1 / y + (3 + x) / (y + 1)");
     }
     
 }
