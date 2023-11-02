2cedc376613e051508aa9d2c3596cda1f0c86c78
David646464 <david.sanchez.peso.64@gmail.com>
2023-11-02 14:24:55 +0100

    commit

--- UD02/src/practicas/Supermercado.java
+++ UD02/src/practicas/Supermercado.java
@@ -3,7 +3,8 @@ package practicas;
 import java.util.Scanner;
 
 public class Supermercado {
-    static String tikect = "===================================================================== \n" + "Nombre producto ----- cantidad ----- precio/unidad ----- precio total \n";
+    static String tikect = "===================================================================== \n"
+            + "Nombre producto ----- cantidad ----- precio/unidad ----- precio total \n";
 
     public static void main(String[] args) {
         supermercado();
@@ -22,12 +23,12 @@ public class Supermercado {
             nombreProducto = sc.next();
             cantidad = -1;
             importe = 0;
-            if (nombreProducto.length() < "Nombre producto ----- ".length()){
-                while (nombreProducto.length() != "Nombre producto ----- ".length()){
+            if (nombreProducto.length() < "Nombre producto ----- ".length()) {
+                while (nombreProducto.length() != "Nombre producto ----- ".length()) {
                     nombreProducto += " ";
                 }
             }
-            
+
             while (cantidad < 0) {
                 System.out.println("Introduce la cantidad");
                 cantidad = sc.nextInt();
@@ -42,10 +43,11 @@ public class Supermercado {
                     System.out.println("El importe introducido no es correcto ya que no puede ser negativo ni 0");
                 }
             }
-            if (importe != -1){
-            importeTotalProducto = (double) Math.round(cantidad * importe * 100) / 100;
-            importeTotal += importeTotalProducto;
-            tikect += nombreProducto  + cantidad + "               " + importe + "               " + importeTotalProducto + "\n";
+            if (importe != -1) {
+                importeTotalProducto = (double) Math.round(cantidad * importe * 100) / 100;
+                importeTotal += importeTotalProducto;
+                tikect += nombreProducto + cantidad + "               " + importe + "               "
+                        + importeTotalProducto + "\n";
             }
         }
         tikect += "Importe total: " + importeTotal + "\n";
@@ -65,7 +67,7 @@ public class Supermercado {
                 }
             }
 
-            calculoDelEfectivo(dineroIntroducido,importeTotal);
+            calculoDelEfectivo(dineroIntroducido, importeTotal);
             tikect += "Metodo de pago: efectivo \n";
             tikect += "===================================================================== \n";
             System.out.println("Gracias por su compra");
@@ -82,22 +84,19 @@ public class Supermercado {
 
     private static void calculoDelEfectivo(double dineroIntroducido, double importeTotal) {
         double cambio = dineroIntroducido - importeTotal;
-        int cambioEntero = (int)(cambio * 100);
+        int cambioEntero = (int) (cambio * 100);
         int cambio50euros = cambioEntero / 5000;
         int cambio20euros = cambioEntero % 5000 / 2000;
-        int cambio10euro= cambioEntero % 5000 % 2000 / 1000;
-        int cambio5euros = cambioEntero  % 1000 / 500;
-        int cambio2euros= cambioEntero %  500 / 200;
-        int cambio1euros= cambioEntero %5 %  200 / 100;
-        int cambio50cent= cambioEntero %  100 / 50;
-        int cambio20cent= cambioEntero %  50 / 20;
-        int cambio10cent= cambioEntero %  50 % 20 / 10;
-        int cambio5cent= cambioEntero %  10 / 5;
-        int cambio2cent= cambioEntero %  5 / 2;
-        int cambio1cent= cambioEntero %5 %  2 / 1;
-
-
-        
+        int cambio10euro = cambioEntero % 5000 % 2000 / 1000;
+        int cambio5euros = cambioEntero % 1000 / 500;
+        int cambio2euros = cambioEntero % 500 / 200;
+        int cambio1euros = cambioEntero % 5 % 200 / 100;
+        int cambio50cent = cambioEntero % 100 / 50;
+        int cambio20cent = cambioEntero % 50 / 20;
+        int cambio10cent = cambioEntero % 50 % 20 / 10;
+        int cambio5cent = cambioEntero % 10 / 5;
+        int cambio2cent = cambioEntero % 5 / 2;
+        int cambio1cent = cambioEntero % 5 % 2 / 1;
 
         tikect += "Dinero ofrecido: " + dineroIntroducido + "\n";
         tikect += "Cambio: " + cambio + "\n";
@@ -113,17 +112,17 @@ public class Supermercado {
         imprimeCambio(cambio5cent, 5, 3);
         imprimeCambio(cambio2cent, 2, 3);
         imprimeCambio(cambio1cent, 1, 3);
-        
+
     }
 
-    private static void imprimeCambio(int cambio, int billeteMoneda,  int indicador) {
-        if (cambio > 0){
-            if (indicador == 1){
-                tikect +=(cambio + " billetes de " + billeteMoneda + " euros"+"\n");
-            }else if (indicador == 2){
-                tikect +=(cambio + " monedas de " + billeteMoneda +" euros"+"\n");
-            }else if (indicador ==3){
-                tikect +=(cambio + " monedas de " + billeteMoneda +" centimos" +"\n");
+    private static void imprimeCambio(int cambio, int billeteMoneda, int indicador) {
+        if (cambio > 0) {
+            if (indicador == 1) {
+                tikect += (cambio + " billetes de " + billeteMoneda + " euros" + "\n");
+            } else if (indicador == 2) {
+                tikect += (cambio + " monedas de " + billeteMoneda + " euros" + "\n");
+            } else if (indicador == 3) {
+                tikect += (cambio + " monedas de " + billeteMoneda + " centimos" + "\n");
             }
         }
     }
